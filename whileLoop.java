8c1fd3f7f7e6d629a6ebd20d75a2fb1479181ed9
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-01 10:04:28 +0700

    Create whileLoop.java

---
+++ whileLoop.java
@@ -0,0 +1,13 @@
+public class whileLoop {
+    public static void main(String[] args) {
+        System.out.println("[---------------------------]");
+        int iwhile = 1;
+        while (iwhile <= 100) {
+            System.out.println(+iwhile);
+            iwhile += 1;
+        
+        }
+        System.out.println(iwhile*50);
+        System.out.println("[---------------------------]");
+    }
+}
