e68faa9795c802a92487751034eccbf6e5132acd
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-01 14:21:54 +0700

    Update testFor.java

--- testFor.java
+++ testFor.java
@@ -1,7 +1,22 @@
 public class testFor {
     public static void main(String[] args) {
-        for(int i = 1 ; i <= 12 ; i++){
-            System.out.println("2"+"\t "+i+"\t "+ 2*i);
+        int a = 1;
+        int X = 1;
+        switch (a) {
+            case 1:
+                X += 5;
+                break;
+            case 2:
+                X += 10;
+                break;
+            case 3:
+                X += 16;
+                break;
+            case 4:
+                X += 34;
+                break;
+            default:
+                break;
         }
     }
 }
