61e8ef0bdb96cae759e3f190b41559276800c27a
KrisdaPhontre <s66309010002@kktech.ac.th>
2024-08-02 16:45:33 +0700

    Update Ex5.java

--- Ex5.java
+++ Ex5.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
 public class Ex5 {
-    
+
+    public static double testAres(double base, double height) {
+        return 0.5 * base * height;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("length: ");
+        double base = scanner.nextDouble();
+        System.out.print("height: ");
+        double height = scanner.nextDouble();
+        double area = testAres(base, height);
+        System.out.printf("area of ​​triangle : ", area);
+    }
 }
