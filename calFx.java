49ae79f88b89d2bec5edec0a52941ea1332a2913
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-02 14:00:09 +0700

    Create calFx.java

---
+++ calFx.java
@@ -0,0 +1,15 @@
+public class calFx {
+    public static void main(String[] args) {
+        for(int i = 0; i <=10 ; i++){
+            System.out.println("Function of "+ i + " = " + fx(i));
+            System.out.println("--------------------");
+        }
+    }
+
+    public static int fx(int x) {
+        int m;
+        m = x * x + 5;
+        System.out.println(""+m);
+        return m;
+    }
+}
