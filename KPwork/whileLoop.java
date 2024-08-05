1c888c0f7ef3f26c5b7ffa26e2f01b4810db3fd3
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-05 10:12:45 +0700

    Create whileLoop.java

---
+++ KPwork/whileLoop.java
@@ -0,0 +1,14 @@
+package KPwork;
+public class whileLoop {
+    public static void main(String[] args) {
+        System.out.println("[---------------------------]");
+        int iwhile = 1, sum = 0;
+        while (iwhile <= 100) {
+            sum += iwhile;
+            iwhile++;
+            System.out.println(+iwhile);
+        }
+        System.out.println("all : "+sum);
+        System.out.println("[---------------------------]");
+    }
+}
