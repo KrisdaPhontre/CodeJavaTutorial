3db42713d9fdd87809b6282c13ed0998dcfac58c
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-05 10:25:42 +0700

    Create Ex53.java

---
+++ Ex53.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Ex53 {
+    public static int findMax(int num1, int num2) {
+        if (num1 > num2) {
+            return num1;
+        } else {
+            return num2;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number #1: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Number #2: ");
+        int num2 = scanner.nextInt();
+        int max = findMax(num1, num2);
+        System.out.println("Maximum value #1 & #2 :" + max);
+    }
+}
