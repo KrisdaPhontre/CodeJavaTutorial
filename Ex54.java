e88faed6eda6a2089b381f7d15e72686b7dfbb7f
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-05 10:25:44 +0700

    Create Ex54.java

---
+++ Ex54.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ex54 {
+    public static String checkEvenOdd(int number) {
+        if (number % 2 == 0) {
+            return "even numbers";
+        } else {
+            return "odd number";
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("PutNumber: ");
+        int number = scanner.nextInt();
+        String result = checkEvenOdd(number);
+        System.out.println("Number: " + result);
+    }
+}
