7f4e9a8a7515e791e4aaa38de2c4ca46724d9501
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-05 10:13:32 +0700

    Delete Squarea.java

--- Squarea.java
+++
@@ -1,7 +0,0 @@
-public class Squarea {
-    public static void main(String[] args) {
-        for(int i = 1 ; i <= 12 ; i++){
-            System.out.println("2"+"\t "+i+"\t "+ 2*i);
-        }
-    }
-}
