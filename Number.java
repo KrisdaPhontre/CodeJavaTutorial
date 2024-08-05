8369e4de9b341982c330e940874830548649a44e
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-05 10:13:23 +0700

    Delete Number.java

--- Number.java
+++
@@ -1,9 +0,0 @@
-public class Number {
-    public static void main(String[] args) {
-        int x,y;
-        x = 62;
-        y = 99;
-        int totel = x+y;
-        System.out.println("Totel = "+totel);
-    }
-}
