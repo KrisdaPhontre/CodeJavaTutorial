8df56eb01ba241d5847f5f331c250e3666f809b0
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-05 10:09:16 +0700

    Create callname.java

---
+++ KPwork/callname.java
@@ -0,0 +1,14 @@
+package KPwork;
+public class callname {
+    public static void main(String[] args) {
+        System.out.println("Hello my name is.");
+        for(int i = 1; i <= 10; i++){
+        Callname();
+        }
+        System.out.println("Back in main.");
+    }
+    public static void Callname() {
+        System.out.println();
+        System.out.println("Krisda Phontre");
+    }
+}
