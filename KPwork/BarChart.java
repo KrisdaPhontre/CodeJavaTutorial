ebde957f3820b6090875ee9abaf7a7f6d3c42447
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-05 10:09:10 +0700

    Create BarChart.java

---
+++ KPwork/BarChart.java
@@ -0,0 +1,16 @@
+package KPwork;
+import java.util.*;
+public interface BarChart {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x;
+        System.out.print("Input Data: ");
+        x = in.nextInt();
+        Bar(x);
+    }
+    public static void Bar(int Data) {
+        for(int i = 1; i <= Data; i++){
+            System.out.print(" * ");
+        }
+    }
+}
