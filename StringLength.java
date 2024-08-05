a60101b4fb2a2adfef6fc40e06bed880916c7b4e
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-05 10:13:39 +0700

    Delete StringLength.java

--- StringLength.java
+++
@@ -1,8 +0,0 @@
-public class StringLength {
-    public static void main(String[] args) {
-        String name = "Computer";
-        int StringSize;
-        StringSize = name.length();
-        System.out.println(name+" has "+ StringSize + " characters");
-    }
-}
