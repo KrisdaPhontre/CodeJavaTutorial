2edd864b0d222a66a3c444e5c26b5704d5c1f78d
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-05 10:11:43 +0700

    Create LocalVars.java

---
+++ KPwork/LocalVars.java
@@ -0,0 +1,16 @@
+package KPwork;
+public class LocalVars {
+    public static void main(String[] args) {
+        phuket();
+        rayoug();
+        phuket();
+    }
+    public static void phuket() {
+        int  birds = 5000;
+        System.out.println("In Phuket there are " +birds+" birds.");
+    }
+    public static void rayoug() {
+        int  birds = 3500;
+        System.out.println("In Rayoug there are " +birds+" birds.");
+    }
+}
