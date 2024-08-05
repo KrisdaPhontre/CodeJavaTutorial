e255ab6be0092f03b2888f6cdb8e1572f6245f87
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-05 10:12:14 +0700

    Create Squarea.java

---
+++ KPwork/Squarea.java
@@ -0,0 +1,8 @@
+package KPwork;
+public class Squarea {
+    public static void main(String[] args) {
+        for(int i = 1 ; i <= 12 ; i++){
+            System.out.println("2"+"\t "+i+"\t "+ 2*i);
+        }
+    }
+}
