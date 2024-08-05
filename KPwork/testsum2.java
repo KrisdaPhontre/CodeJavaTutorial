647314ce300e53c38c2cfd228c5622169fd6ba00
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-05 10:12:43 +0700

    Create testsum2.java

---
+++ KPwork/testsum2.java
@@ -0,0 +1,13 @@
+package KPwork;
+public class testsum2 {
+    public static void main(String[] args) {
+        int i,sum = 0;
+        for(i=1; i<=100; i++){
+            if (((i % 3) ==0) || ((i % 5)==0)) {
+                System.out.println(i+" ");
+                sum += i;
+
+            }
+        }
+    }
+}
