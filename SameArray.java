b4193062be28a5be274bd52bcd0916a0d1f1389a
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-05 10:23:50 +0700

    Update SameArray.java

---
+++ SameArray.java
@@ -0,0 +1,13 @@
+public class SameArray {
+    public static void main(String[] args) {
+        int[] array1 = { 2 , 4 , 6 , 8 , 10 };
+        int[] array2 = array1;
+        array1[0] = 200;
+        array2[4] = 1000;
+        System.out.println("The contents of array2 :");
+        for(int index = 0;index < array2.length;index++){
+            System.out.println(array2[index]+ " ");
+            System.out.println();
+        }
+    }
+}
