0f15dbe7e09aafe9ffc39da3988f45f4eb6441a8
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-05 10:36:01 +0700

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,10 @@
+package KPwork;
+public class Number {
+    public static void main(String[] args) {
+        int x,y;
+        x = 62;
+        y = 99;
+        int totel = x+y;
+        System.out.println("Totel = "+totel);
+    }
+}
