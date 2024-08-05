798dc7f1a8e36573cb739158ef66a63d7adfbe03
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-05 10:08:57 +0700

    Delete ExNo2.java

--- ExNo2.java
+++
@@ -1,9 +0,0 @@
-public class ExNo2 {
-    public static void main(String[] args) {
-        for(int i =1;i<=100;i++){
-            if(((i % 3)==0)||(i%5)==0){
-                System.out.println(i);
-            }
-        }
-    }
-}
