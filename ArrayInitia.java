b7c37c9737ac2d0e2076f982a877a58674254785
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-05 10:11:26 +0700

    Update ArrayInitia.java

--- ArrayInitia.java
+++ ArrayInitia.java
@@ -2,7 +2,7 @@ public class ArrayInitia {
     public static void main(String[] args) {
         int [] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         for(int index = 0; index < 12; index++){
-            System.out.println("MOnth" + (index + 1) + " has" + days[0] + " Days.");
+            System.out.println("MOnth" + (index + 1) + " has" + days[index] + " Days.");
         }
     }
 }
