8711b532e208bb69ce7c762e46f0c7762e30c223
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-05 15:17:59 +0700

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ArrayList {
+    public static void main(String[] args) {
+        ArrayList nameList = new ArrayList();
+        nameList.add("Aom");
+        nameList.add("Java");
+        nameList.add("Micro");
+        System.out.println("The ArrayList Has " + nameList.size() + " objects stored in it.");
+        for(int index = 0; index < number.size(); index++){
+            System.out.println(nameList.get(index));
+        }
+    }
+}
