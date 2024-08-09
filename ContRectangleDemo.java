910c55ac19248b8fa1cc6bd1ea0474251f3242e0
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-09 12:18:18 +0700

    Create ContRectangleDemo.java

---
+++ ContRectangleDemo.java
@@ -0,0 +1,11 @@
+public class ContRectangleDemo {
+    public static void main(String[] args) {
+        ContRectangle box = new ContRectangle(8.0,12.0);
+        System.out.println("The box's length is "+ boxA.getLength);
+        System.out.println("The box's length is "+ boxA.getWidth);
+        System.out.println("The box's length is "+ boxA.getArea);
+        System.out.println("The box's length is "+ boxB.getLength);
+        System.out.println("The box's length is "+ boxB.getWidth);
+        System.out.println("The box's length is "+ boxB.getArea);
+    }
+}
