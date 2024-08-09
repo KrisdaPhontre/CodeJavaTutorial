44b91c29c8c2fbd77070407edcd3b1f0ea099630
KrisdaPhontre <152252380+KrisdaPhontre@users.noreply.github.com>
2024-08-09 12:18:29 +0700

    Create Studen1.java

---
+++ Studen1.java
@@ -0,0 +1,20 @@
+public class Studen1 {
+    private String name ;
+    private String Email;
+    public Studen1(){
+        name = "Unassigned";
+        Email = "Unassigned";
+    }
+    public void setName1(String studenName){
+        name = studenName;
+    }
+    public void setEmail1(String address){
+        Email = address;
+    }
+    public String getName1(){
+        return name;
+    }
+    public String getEmail1(){
+        return Email;
+    }
+}
